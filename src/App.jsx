06d67f39a3962e33e38d5e44c574e94459881be4
sopanha09 <sopanhasous@gmail.com>
2025-09-05 12:52:22 +0700

    remove ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './views/Home';
-import Ingredients from './views/Ingredients';
-import MealsByIngredient from './views/MealsByIngredient';
-import MealDetails from './views/MealDetails';
-import Login from './auth/views/Login';
-import ProtectedRoute from './auth/components/ProtectedRoute';
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Ingredients from "./views/Ingredients";
+import MealsByIngredient from "./views/MealsByIngredient";
+import MealDetails from "./views/MealDetails";
+import Login from "./auth/views/Login";
+import ProtectedRoute from "./auth/components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/ingredients"
-        element={
-          <ProtectedRoute>
-            <Ingredients />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/ingredient/:ingredient"
-        element={
-          <ProtectedRoute>
-            <MealsByIngredient />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/meal/:id"
-        element={
-          <ProtectedRoute>
-            <MealDetails />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/" element={<Home />} />
+      <Route path="/ingredients" element={<Ingredients />} />
+      <Route path="/ingredient/:ingredient" element={<MealsByIngredient />} />
+      <Route path="/meal/:id" element={<MealDetails />} />
     </Routes>
   );
 }
